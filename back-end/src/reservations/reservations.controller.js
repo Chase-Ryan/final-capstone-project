03fa8fb885aacc93ab37db277830dfb3c7ca6af3
Chase Ryan <Chasemryan1211@gmail.com>
2022-01-14 13:09:16 -0600

    added create to res controller

--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -3,27 +3,31 @@
  */
 const asyncErrorBoundary = require('../errors/asyncErrorBoundary');
 const service = require('./reservations.service');
+const hasProperties = require('../errors/hasProperties');
 //complete list function
 //valid date middleware
-
+const hasRequiredProperties = hasProperties(
+  "first_name",
+  "last_name",
+  "mobile_number",
+  "reservation_date",
+  "reservation_time",
+  "people"
+)
 
 async function list(req, res) {
   const {date} = req.query;
-  let reservations;
-  reservations = date ? await service.listByDate(date) : await service.list();
-  res.json({
-    data: reservations,
-  });
+  let data;
+  data = date ? await service.listByDate(date) : await service.list();
+  res.json({ data });
 }
 
 async function create(req, res) {
-  const reservation = req.body.data;
-  const { reservation_id } = await service.create(reservation);
-  reservation.reservation_id = reservation_id;
-  res.status(201).json({ data: reservation });
+  const data = await service.create(req.body.data);
+  res.status(201).json({ data });
 }
 
 module.exports = {
   list: asyncErrorBoundary(list),
-  create: asyncErrorBoundary(create),
+  create: [hasRequiredProperties, asyncErrorBoundary(create)],
 };
